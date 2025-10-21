1c25c790c8243ed9db4d9950433adfd013264873
sandstranger <sandstranger@gmail.com>
2025-10-21 10:53:06 +0400

    always disable angle

--- src/gl/init.c
+++ src/gl/init.c
@@ -80,7 +80,7 @@ void set_getprocaddress(void*(APIENTRY_GL4ES* new_proc_address)(const char*)) {
 void init_internal_glDrawElementsBaseVertex();
 void initialize_ng_gl4es() {
     
-    int enableANGLE = config_get_int("enableANGLE");
+    int enableANGLE = 0;// config_get_int("enableANGLE");
     globals4es.enableANGLE = enableANGLE;
     if (enableANGLE) {
         setenv("LIBGL_GLES", "libGLESv2_angle.so", 1);
