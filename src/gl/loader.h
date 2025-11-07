5f07a42ee92fb632a42c8e2b92e3871cad5e23b4
Nadezhda Samartseva <nadya.sm101@yandex.ru>
2025-11-07 03:14:22 +0300

    [FIX] implementing the use of thread local through macros

--- src/gl/loader.h
+++ src/gl/loader.h
@@ -146,6 +146,19 @@ extern "C"
 
 #define PUSH_IF_COMPILING(name) PUSH_IF_COMPILING_EXT(name, name##_ARG_NAMES)
 
+#if defined(_WIN32) || defined(_WIN64)
+  #define THREAD_LOCAL __declspec(thread)
+#elif (__STDC_VERSION__ >= 201112L) // >= C11
+  #define THREAD_LOCAL _Thread_local
+#elif (__STDC_VERSION__ > 201710L) // > C23
+  #undef THREAD_LOCAL
+  #define THREAD_LOCAL thread_local
+#elif defined (__GCC__) || defined(__clang__)
+  #define THREAD_LOCAL __thread
+#else
+  #define THREAD_LOCAL 
+#endif
+
 #define DEFINE_RAW(lib, name) static name##_PTR lib##_##name = NULL
 #define LOAD_RAW(lib, name, ...)                                                                                       \
     {                                                                                                                  \
