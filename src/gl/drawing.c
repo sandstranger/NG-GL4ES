0a233be1744d22efddbcc99777896bf462c2d1f9
Evgeniy Mineev <e.mineev@crazypandagames.com>
2025-11-21 23:07:58 +0400

    fix compiling after cherry picks

--- src/gl/drawing.c
+++ src/gl/drawing.c
@@ -518,25 +518,24 @@ void APIENTRY_GL4ES gl4es_glDrawRangeElements(GLenum mode, GLuint start, GLuint 
       if (glstate->vao->elements) {
         src = (void*)(char*)glstate->vao->elements->data + (uintptr_t)indices;
 	  } else {
-			src = (GLvoid *) indices[i];
+			src = (GLvoid*)indices;
         }
       sindices = copy_gl_array(src, type, 1, 0, GL_UNSIGNED_SHORT, 1, 0, count, NULL);  
-      old_index = wantBufferIndex(0);
     } else {
               if(type==GL_UNSIGNED_INT) {
                   if (glstate->vao->elements) {
                       iindices = (void*)(char*)glstate->vao->elements->data + (uintptr_t)indices;
                   } else {
-                      iindices = (GLuint *) indices[i];
+                      iindices = (GLvoid*)indices;
                   }
               }
               else {
                   if (glstate->vao->elements) {
                       sindices = (void*)(char*)glstate->vao->elements->data + (uintptr_t)indices;
                   } else {
-                      sindices = (GLushort *) indices[i];
+                      sindices = (GLvoid*)indices;
                   }
-              }                                                : (GLvoid*)indices;
+              }
     }
 
     if (compiling) {
